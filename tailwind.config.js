991b0af2870acc536dda96d3918e33a4bc807ecd
Kamil Mackow <kamilmackow98@gmail.com>
2022-09-04 19:52:34 +0200

    build: update PostCSS related config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { screens } = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -5,7 +7,24 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    screens: {
+      xxs: '420px',
+      xs: '480px',
+      ...screens,
+      // sm: '640px',
+      // md: '768px',
+      // lg: '1024px',
+      // xl: '1280px',
+      // '2xl': '1536px',
+    },
     extend: {},
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        lg: '2rem',
+        '2xl': '8rem',
+      },
+    },
   },
   plugins: [],
 };
