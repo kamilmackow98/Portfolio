4bf90f2441df73f4959f878fd3a4f7924b77e0bf
Kamil Mackow <kamilmackow98@gmail.com>
2022-11-02 00:07:09 +0100

    feat(content): Add content on Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,35 @@ import type { NextPage } from 'next'
 import CustomHead from '@atoms/CustomHead'
 
 const Home: NextPage = () => {
-  return <>
-    <CustomHead title='Home' />
-  Home page</>
+  return (
+    <>
+      <CustomHead title="Home" />
+      <div className="page-container">
+        <div className="content">
+          <h1>I build unique experiences</h1>
+          <p>
+            I&#39;m a Web Developer with a passion for UI and UX. Mainly focused
+            on front-end development, I like to design user interfaces, create
+            stunning websites and develop cutting-edge applications.
+          </p>
+
+          <div id="home-buttons-wrapper">
+            <button>Check my work</button>
+            <div>OR</div>
+            <button>Learn about me</button>
+          </div>
+
+          <div id="resume-wrapper">
+
+          </div>
+        </div>
+
+        <div className="placeholder">
+          <div>Spinning circles.</div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Home
