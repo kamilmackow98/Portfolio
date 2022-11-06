ceb75d23d9826c953de8401b5b03c098f4f79607
Kamil Mackow <kamilmackow98@gmail.com>
2022-11-06 14:39:53 +0100

    feat(molecules): Add Tech used component

---
+++ components/common/molecules/TechUsed.tsx
@@ -0,0 +1,99 @@
+import {
+  FaReact as ReactIcon,
+  FaSymfony as SymfonyIcon,
+  FaNodeJs as NodeIcon,
+  FaSass as SassIcon,
+  FaGitAlt as GitIcon,
+} from 'react-icons/fa'
+import {
+  SiVuedotjs as VueIcon,
+  SiTypescript as TypeScriptIcon,
+  SiJavascript as JavaScriptIcon,
+  SiPhp as PhpIcon,
+} from 'react-icons/si'
+import { CgFigma as FigmaIcon } from 'react-icons/cg'
+import { IconType } from 'react-icons'
+import TechBlock from '@atoms/TechBlock/TechBlock'
+
+export type Tech = {
+  id: string
+  name: string
+  icon: IconType
+}
+
+const tech: Tech[] = [
+  {
+    id: 'react',
+    name: 'React',
+    icon: ReactIcon,
+  },
+  {
+    id: 'vue',
+    name: 'Vue',
+    icon: VueIcon,
+  },
+  {
+    id: 'symfony',
+    name: 'Symfony',
+    icon: SymfonyIcon,
+  },
+  {
+    id: 'typescript',
+    name: 'TypeScript',
+    icon: TypeScriptIcon,
+  },
+  {
+    id: 'javascript',
+    name: 'JavaScript',
+    icon: JavaScriptIcon,
+  },
+  {
+    id: 'node',
+    name: 'Node',
+    icon: NodeIcon,
+  },
+  {
+    id: 'php',
+    name: 'PHP',
+    icon: PhpIcon,
+  },
+  {
+    id: 'scss',
+    name: 'SCSS',
+    icon: SassIcon,
+  },
+  {
+    id: 'git',
+    name: 'Git',
+    icon: GitIcon,
+  },
+  {
+    id: 'figma',
+    name: 'Figma',
+    icon: FigmaIcon,
+  },
+]
+
+const TechUsed = () => {
+  const renderTechFirstRow = tech
+    .slice(0, 3)
+    .map((t) => <TechBlock key={t.id} {...t} />)
+
+  const renderTechSecondRow = tech
+    .slice(3, 7)
+    .map((t) => <TechBlock key={t.id} {...t} />)
+
+  const renderTechThirdRow = tech
+    .slice(7, tech.length)
+    .map((t) => <TechBlock key={t.id} {...t} />)
+
+  return (
+    <div id="tech" className="flex flex-col justify-center items-center">
+      <div className="flex">{renderTechFirstRow}</div>
+      <div className="flex mt-2">{renderTechSecondRow}</div>
+      <div className="flex mt-2">{renderTechThirdRow}</div>
+    </div>
+  )
+}
+
+export default TechUsed
