b1ee5b551f49eb7de52c1b23b2b63902f2506795
Kamil Mackow <kamilmackow98@gmail.com>
2022-11-07 23:59:07 +0100

    feat(atoms): Add Current event component

---
+++ components/common/atoms/Timeline/CurrentEvent.tsx
@@ -0,0 +1,29 @@
+import { HTMLAttributes } from 'react'
+import { Event } from './Event'
+import styles from './CurrentEvent.module.css'
+import classNames from 'classnames'
+
+interface Props extends HTMLAttributes<HTMLDivElement> {
+  event: Event
+}
+
+const CurrentEvent = (props: Props) => {
+  const {
+    event: { title, description, dates },
+    className,
+    ...rest
+  } = props
+
+  const classes = classNames(styles.wrapper, className)
+
+  return (
+    <div className={classes} {...rest}>
+      <div className="mb-2 text-white theme-font--heading font-semibold font-text--lg tracking-wide">
+        {title}
+      </div>
+      <div className="theme-font--reading font-medium">{description}</div>
+    </div>
+  )
+}
+
+export default CurrentEvent
