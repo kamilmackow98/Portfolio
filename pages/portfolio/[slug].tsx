d9045ab4234a070bb855f7d674de4590bfa42693
Kamil Mackow <kamilmackow98@gmail.com>
2022-11-14 00:48:24 +0100

    feat(pages): Finish main features and styling on Project page

--- pages/portfolio/[slug].tsx
+++ pages/portfolio/[slug].tsx
@@ -1,24 +1,82 @@
 import CustomHead from '@atoms/CustomHead'
+import NoScrollLink from '@atoms/NoScrollLink'
+import Tag from '@atoms/Tag/Tag'
 import { getProject, getProjects } from '@utils/mdx'
+import clsx from 'clsx'
 import { getMDXComponent } from 'mdx-bundler/client'
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
 import { ParsedUrlQuery } from 'querystring'
 import { useMemo } from 'react'
 import { Project as ProjectType } from 'types'
+import { BiArrowBack } from 'react-icons/bi'
+import styles from '@styles/pages/Project.module.css'
+import Image from '@atoms/Markdown/Image'
+import Link from '@atoms/Link/Link'
+import { FiExternalLink } from 'react-icons/fi'
+import InternalLink from '@atoms/Markdown/InternalLink'
 
 interface Props extends ProjectType {}
 
+const components = {
+  a: Link,
+  InternalLink,
+  Image,
+}
+
 const Project: NextPage<Props> = (project: Props) => {
   const { code, frontmatter } = project
-  const { title, excerpt } = frontmatter
+  const { title, excerpt, tags, link } = frontmatter
 
   const Component = useMemo(() => getMDXComponent(code), [code])
+
+  const renderTags = tags.map((tag, idx) => (
+    <li className={clsx('peer peer-first:ml-2.5')} key={idx}>
+      <Tag className="px-4 py-2">{tag}</Tag>
+    </li>
+  ))
+
+  const renderLink = link ? (
+    <a className="ml-2" href={link} target="_blank" rel="noopener noreferrer">
+      <FiExternalLink size={20} className="text-white" />
+    </a>
+  ) : null
 
   return (
     <>
       <CustomHead title={`${title} | Portfolio`} description={excerpt} />
-      <h1>{title}</h1>
-      <Component components={{}} />
+      <div id="project-page" className="page-container">
+        <div className="flex items-center">
+          <BiArrowBack size={18} />
+          <NoScrollLink
+            className={clsx(
+              'theme-font--reading font-text--md font-medium',
+              'tracking-wide',
+              'ml-2',
+            )}
+            href="/portfolio"
+          >
+            All projects
+          </NoScrollLink>
+        </div>
+        <div className={clsx('flex', 'items-center', 'mt-8')}>
+          <h2
+            className={clsx(
+              'theme-font--heading font-text--xxl font-semibold',
+              'tracking-wide text-white',
+            )}
+          >
+            {title}
+          </h2>
+          {renderLink}
+        </div>
+        <ul className={clsx('flex items-center', 'mt-3')}>
+          {renderTags}
+          <span className={clsx(styles['tags-separator'], 'ml-8')} />
+        </ul>
+        <div className={clsx(styles['md-content'], 'mt-24')}>
+          <Component components={components} />
+        </div>
+      </div>
     </>
   )
 }
