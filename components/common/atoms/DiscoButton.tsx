e3a01f3396a63f39698f61c48abdd16a2f7bad49
Kamil Mackow <kamilmackow98@gmail.com>
2022-11-20 23:53:55 +0100

    feat(atoms): Fix Disco Buttons and Links appearance (svg rendering)

--- components/common/atoms/DiscoButton.tsx
+++ components/common/atoms/DiscoButton.tsx
@@ -24,8 +24,8 @@ const DiscoButton = (props: Props) => {
           fillOpacity="0.05"
           stroke={variantColorRef.current}
           strokeOpacity="0.8"
-          strokeWidth="0.25"
-          d="M 30.05126,0.12579455 H 0.12805072 V 7.0952359 L 4.9487577,9.8741902 H 34.871968 V 2.9047488 Z"
+          strokeWidth="0.3"
+          d="M 30.05126,0.12579455 H 0.12805072 V 7.0952359 L 4.9487577,9.8741902 H 34.751968 V 2.9047488 Z"
           id="btn-bg"
         />
       </g>
@@ -42,8 +42,8 @@ const DiscoButton = (props: Props) => {
           fillOpacity="0.05"
           stroke={variantColorRef.current}
           strokeOpacity="0.8"
-          strokeWidth="0.25"
-          d="M 4.9487583,0.12579455 H 34.871968 V 7.0952359 L 30.051261,9.8741902 H 0.12805072 V 2.9047488 Z"
+          strokeWidth="0.3"
+          d="M 4.9487583,0.12579455 H 34.751968 V 7.0952359 L 30.051261,9.8741902 H 0.25805072 V 2.9047488 Z"
           id="btn-bg"
         />
       </g>
