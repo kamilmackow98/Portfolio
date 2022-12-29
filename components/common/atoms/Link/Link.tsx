69404b5e8e14e59041e89b8a910dbd99dc622ed6
Kamil Mackow <kamilmackow98@gmail.com>
2022-12-29 01:15:56 +0100

    feat(styling): Use Stitches instead of CSS modules

--- components/common/atoms/Link/Link.tsx
+++ components/common/atoms/Link/Link.tsx
@@ -1,6 +1,20 @@
 import { AnchorHTMLAttributes } from 'react'
-import styles from './Link.module.css'
-import clsx from 'clsx'
+import { styled } from '@stitches/react'
+
+export const StyledLink = styled('a', {
+  color: 'var(--theme-primary)',
+  position: 'relative',
+  backgroundImage:
+    'linear-gradient(to right, var(--theme-primary), var(--theme-primary))',
+  backgroundPosition: 'bottom left',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: '0% 1px',
+
+  transition: 'background 500ms cubic-bezier(0.25, 1, 0.35, 1)',
+  '&:hover, &:focus-visible': {
+    backgroundSize: '100% 1px',
+  },
+})
 
 interface Props extends AnchorHTMLAttributes<HTMLAnchorElement> {}
 
@@ -8,14 +22,14 @@ const Link = (props: Props) => {
   const { children, className, ...rest } = props
 
   return (
-    <a
-      className={clsx(styles.wrapper, className)}
+    <StyledLink
+      className={className}
       rel="noopener noreferrer"
       target="_blank"
       {...rest}
     >
       {children}
-    </a>
+    </StyledLink>
   )
 }
 
