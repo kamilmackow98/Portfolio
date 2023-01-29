5e26eaca56d14e9b01fa096e7017fc6e15c090d8
Kamil Mackow <kamilmackow98@gmail.com>
2023-01-29 04:30:00 +0100

    feat(pages): Add custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,26 @@
+import CustomHead from '@components/misc/CustomHead'
+import { cx } from 'class-variance-authority'
+import { NextPage } from 'next'
+
+const pageClasses = cx(
+  'flex flex-1 justify-center items-center',
+  'my-20 lg:my-48',
+  'page-container',
+)
+const titleClasses = cx(
+  'text-white tracking-wider font-medium',
+  'theme-font--heading font-text--xl',
+)
+
+const Custom404: NextPage = () => {
+  return (
+    <>
+      <CustomHead title="Page not found" />
+      <div className={pageClasses}>
+        <h1 className={titleClasses}>404 - Page not found.</h1>
+      </div>
+    </>
+  )
+}
+
+export default Custom404
