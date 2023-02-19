b120eed7c656707fb8a1474815dbf562fef236e7
Kamil Mackow <kamilmackow98@gmail.com>
2023-02-19 14:20:19 +0100

    chore(config): Make Tailwind support MDX files when adding classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,9 @@ const { screens, fontSize } = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './data/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     fontSize: {
